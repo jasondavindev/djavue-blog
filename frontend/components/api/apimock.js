e531f930aec88f4dda745c794904f83fe08c2cfb
jasondavindev <jasondavin.dev@gmail.com>
2019-06-15 16:58:34 -0300

    adicionado alguns metodos da apimock

--- frontend/components/api/apimock.js
+++ frontend/components/api/apimock.js
@@ -1,51 +1,87 @@
-import Vue from 'vue'
+let logged_user = null;
 
-var logged_user = null;
-
-function mockasync (data) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve({data: data}), 600)
-  })
+function mockasync(data) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => resolve({ data: data }), 600);
+	});
 }
 
 const api = {
-    login(username, password){
-        if(password){
-            logged_user = {
-                username: username,
-                first_name: 'Mark',
-                last_name: 'Zuckerberg',
-                email: 'zuck@facebook.com',
-                notifications_enabled: true,
-                permissions:{
-                    ADMIN: username == 'admin',
-                    STAFF: username == 'admin',
-                }
-            };
-        }
-        return mockasync(logged_user);
-    },
-    logout(){
-        logged_user = null;
-        return mockasync({});
-    },
-    whoami(){
-        return mockasync(logged_user ? {
-            authenticated: true,
-            user: logged_user,
-        } : {authenticated: false});
-    },
-    add_todo(newtask){
-        return mockasync({description: newtask, done: false});
-    },
-    list_todos(){
-        return mockasync({
-            todos: [
-                {description: 'Do the laundry', done: true},
-                {description: 'Walk the dog', done: false}
-            ]
-        });
-    }
+	login(username, password) {
+		if (password) {
+			logged_user = {
+				username: username,
+				first_name: 'Django',
+				last_name: 'Bot',
+				email: 'bot@mail.com',
+				notifications_enabled: true,
+				permissions: {
+					ADMIN: username == 'admin',
+					STAFF: username == 'admin',
+				},
+			};
+		}
+		return mockasync(logged_user);
+	},
+	logout() {
+		logged_user = null;
+		return mockasync({});
+	},
+	whoami() {
+		return mockasync(
+			logged_user
+				? {
+						authenticated: true,
+						user: logged_user,
+					}
+				: { authenticated: false }
+		);
+	},
+	add_todo() {},
+	list_todos() {},
+	list_posts() {
+		return mockasync({
+			posts: [
+				{
+					title: 'My first post',
+					body: 'This is my first post',
+					author: {
+						username: 'bot',
+						first_name: 'Django',
+						last_name: 'Bot',
+					},
+					created: Date.now(),
+				},
+				{
+					title: 'Random post',
+					body: 'Random content',
+					author: {
+						username: 'bot',
+						first_name: 'Django',
+						last_name: 'Bot',
+					},
+					created: Date.now(),
+				},
+			],
+		});
+	},
+	create_post(post) {
+		return mockasync(
+			Object.assign(
+				{
+					data: post,
+				},
+				{
+					created: Date.now(),
+					author: {
+						username: logged_user.username,
+						first_name: logged_user.first_name,
+						last_name: logged_user.last_name,
+					},
+				}
+			)
+		);
+	},
 };
 
 export default api;
